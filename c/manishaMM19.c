b117b498b357eed70789df0ee288cf1dcd6bfcad
Manisha <jayathissagrmm.19@uom.lk>
2022-10-01 13:35:59 +0530

    Commit 2

--- c/manishaMM19.c
+++ c/manishaMM19.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 int main(){
-    //Sum of odd numbers
-    int max, sum = 0;
-    printf("Enter the maximum value: ");
-    scanf("%d", &max);
-
-    for (int i = 1; i <= max; i++ ){
-        if (i % 2 != 0){
-            sum = sum + i;
+     
+    //Count Ones in a 2D Array
+      int count=0;
+     int num[2][3]= {{1,3,1},{2,1,7}};
+     for(int i=0; i<2; i++){
+        for(int j=0; j<3; j++){
+            if(num[i][j]==1){
+                count ++;
+            }
         }
-    }
-    printf("Sum of Odd Numbers between 0 and %d = %d", max, sum);
+     }
+     printf("%d",count);
     return 0;
 }
