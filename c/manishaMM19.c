4d8ce792c0aec7e0f013b54f9190420b6b23b52e
Manisha <jayathissagrmm.19@uom.lk>
2022-10-01 16:58:40 +0530

    Commit Sum of odd number

--- c/manishaMM19.c
+++ c/manishaMM19.c
@@ -12,6 +12,18 @@ int main(){
         }
      }
      printf("%d",count);
+
+     //Sum of odd numbers
+      int max, sum = 0;
+    printf("Enter the maximum value: ");
+    scanf("%d", &max);
+
+    for (int i = 1; i <= max; i++ ){
+        if (i % 2 != 0){
+            sum = sum + i;
+        }
+    }
+    printf("Sum of Odd Numbers between 0 and %d = %d", max, sum);
     return 0;
 
    
